332111567c3ef454b667e6653dfe8b02b8de06f1
sgdavis <kingpreist@hotmail.com>
2012-10-30 13:23:03 -0600

    Adding imports to service manager to use the required libraries
    done via github, rather than the terminal commit. Not sure how well it worked.

--- AndroidProject/workspace/TaskTracker/src/com/example/tasktracker/ServiceManager.java
+++ AndroidProject/workspace/TaskTracker/src/com/example/tasktracker/ServiceManager.java
@@ -1,6 +1,22 @@
 package com.example.tasktracker;
 
-
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Type;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import com.google.gson.Gson;
 
 public class ServiceManager
 {

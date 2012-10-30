bb5991f80ec0ceb946a1e7209e226ba480aa2b75
efauser <efauser@ualberta.ca>
2012-10-29 19:58:43 -0600

    Initial work on Task, Fulfillment and TaskManager classes.

--- AndroidProject/workspace/TaskTracker/src/com/example/tasktracker/Task.java
+++ AndroidProject/workspace/TaskTracker/src/com/example/tasktracker/Task.java
@@ -1,22 +1,145 @@
 package com.example.tasktracker;
 
+import java.io.File;
 import java.util.ArrayList;
+
+
 
 
 public class Task
 {
 	private String taskName;
 	private String taskDescription;
-	private String userName;
-	private String userID;
 	
-	private Byte flags; //stores booleans in the form 0b(photo)(text)(audio)(open)(public)000
+	private byte flags; //stores booleans in the form 0b(photo)(text)(audio)(open)(public)000
 	
-	private int Identification;
+	private String userDeviceId;
 	
 	private ArrayList<Fulfillment> submissions;
 	
-	Task()
-	{
+	Task(String ownerId, String name, String desc, boolean wantText, boolean wantPhoto, boolean wantAudio, boolean isPublic){
+		this.taskName = name;
+		this.taskDescription = desc;
+		this.setWantText(wantText);
+		this.setWantPhoto(wantPhoto);
+		this.setWantAudio(wantAudio);
+		this.setIsPublic(isPublic);
+		this.setIsOpen(true);
+		this.userDeviceId = ownerId;
+		this.submissions = new ArrayList<Fulfillment>();
+	}
+	
+	public String getUserDeviceId(){
+		return userDeviceId;
+	}
+	
+	public void setUserDeviceId(String deviceId){
+		this.userDeviceId = deviceId;
+	}
+	
+	public String getTaskName(){
+		return taskName;
+	}
+	
+	public void setTaskName(String name){
+		this.taskName = name;
+	}
+	
+	public String getTaskDescription(){
+		return taskDescription;
+	}
+	
+	public void setTaskDescription(String desc){
+		this.taskDescription = desc;
+	}
+	
+	public boolean getWantPhoto(){
+		if(((flags >>> 7) & 1) == 1){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	public void setWantPhoto(boolean text){
+		if (text){
+			this.flags = (byte) (flags | (1 << 7));
+		}else{
+			this.flags = (byte) (flags & ~(1 << 7));
+		}
+	}
+	
+	public boolean getWantText(){
+		if(((flags >>> 6) & 1) == 1){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	public void setWantText(boolean text){
+		if (text){
+			this.flags = (byte) (flags | (1 << 6));
+		}else{
+			this.flags = (byte) (flags & ~(1 << 6));
+		}
+	}
+	
+	public boolean getWantAudio(){
+		if(((flags >>> 5) & 1) == 1){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	public void setWantAudio(boolean audio){
+		if (audio){
+			this.flags = (byte) (flags | (1 << 5));
+		}else{
+			this.flags = (byte) (flags & ~(1 << 5));
+		}
+	}
+	
+	public boolean getIsOpen(){
+		if(((flags >>> 4) & 1) == 1){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	public void setIsOpen(boolean open){
+		if (open){
+			this.flags = (byte) (flags | (1 << 4));
+		}else{
+			this.flags = (byte) (flags & ~(1 << 4));
+		}
+	}
+	
+	public boolean getIsPublic(){
+		if(((flags >>> 3) & 1) == 1){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	public void setIsPublic(boolean pub){
+		if (pub){
+			this.flags = (byte) (flags | (1 << 3));
+		}else{
+			this.flags = (byte) (flags & ~(1 << 3));
+		}
+	}
+	
+	
+	public void addSubmission(String ownerId, String text, ArrayList<File> images, ArrayList<File> audio){
+		Fulfillment ful = new Fulfillment(ownerId, text,images, audio);
+		this.submissions.add(ful);
+	}
+	
+	public void removeSubmission(int index){
+		this.submissions.remove(index);
 	}
 }
--- AndroidProject/workspace/TaskTracker/src/com/example/tasktracker/TaskManager.java
+++ AndroidProject/workspace/TaskTracker/src/com/example/tasktracker/TaskManager.java
@@ -1,26 +1,43 @@
 package com.example.tasktracker;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import android.provider.Settings.Secure;
+import android.content.Context;
 
 
-public class TaskManager
-{
+public class TaskManager{
+	
+	private static final String FILE_NAME = "tasks.sav";
+	
 	private ArrayList<Task> TaskList;
+	
+	private Context context;
+	
+	private String userId; 
 	
 	private static TaskManager instance = null;
 	
-	protected TaskManager()
-	{
+	protected TaskManager(Context context){
+		this.TaskList = new ArrayList<Task>();
+		this.context = context;
+		this.userId = Secure.getString(context.getContentResolver(), Secure.ANDROID_ID); 
 	}
 	
-	public static TaskManager getInstance(int type)
+	public static TaskManager getInstance(int type, Context context)
 	{
 		TaskManager ret = null;
-		if( type == 1)//change if we need to acomodate multiple servers
+		if( type == 1)//change if we need to accommodate multiple servers
 		{
 			if(instance == null)
 			{
-				instance = new TaskManager();
+				instance = new TaskManager(context);
 			}
 			
 			ret = instance;
@@ -29,35 +46,88 @@ public class TaskManager
 		return ret;
 	}
 	
-	public void updateName()
-	{
+	public void updateName(int index, String name){
+		TaskList.get(index).setTaskName(name);
+		saveTasks();
 	}
 	
-	public void updateDesc()
-	{
+	public void updateDesc(int index, String desc){
+		TaskList.get(index).setTaskDescription(desc);
+		saveTasks();
 	}
 	
-	public void updateRequirements()
-	{
+	public void updateRequirements(int index, boolean text, boolean photo, boolean audio){
+		TaskList.get(index).setWantText(text);
+		TaskList.get(index).setWantPhoto(photo);
+		TaskList.get(index).setWantAudio(audio);
+		saveTasks();
 	}
 	
-	public void updateOpen()
-	{
+	public void updateOpen(int index, boolean open){
+		TaskList.get(index).setIsOpen(open);
+		saveTasks();
 	}
 	
-	public void updatePublic()
-	{
+	public void updatePublic(int index, boolean pub){
+		TaskList.get(index).setIsPublic(pub);
+		saveTasks();
 	}
 	
-	public void cancel() // ?
-	{
+	public void createTask(String name, String desc, boolean wantText, boolean wantPhoto, boolean wantAudio, boolean isPublic){
+		Task newTask = new Task(userId, name, desc, wantText, wantPhoto, wantAudio, isPublic);
+		TaskList.add(newTask);
+		saveTasks();
 	}
 	
-	public void createTask()
-	{
+	public void deleteTask(int index){
+		TaskList.remove(index);
+		saveTasks();
 	}
 	
-	public void deleteTask()
-	{
+	public void addSubmission(int index, String text, ArrayList<File> images, ArrayList<File> audio){
+		TaskList.get(index).addSubmission(userId, text, images, audio);
+		saveTasks();
+	}
+	
+	public void removeSubmission(int taskIndex, int subIndex){
+		TaskList.get(taskIndex).removeSubmission(subIndex);
+		saveTasks();
+	}
+	
+	public ArrayList<Task> getTaskList(){
+		this.TaskList = loadTasks();
+		//Eventually load public tasks from service manager
+		return TaskList;
+	}
+	
+	private void saveTasks(){
+		 try {  
+	            FileOutputStream fOut = context.openFileOutput(FILE_NAME, Context.MODE_PRIVATE);
+	        	ObjectOutputStream oOut = new ObjectOutputStream(fOut);    
+	            oOut.writeObject(TaskList);
+	            oOut.flush();
+	            oOut.close();  
+	        } catch (FileNotFoundException e) {  
+	            e.printStackTrace();  
+	        } catch (IOException e) {  
+	            e.printStackTrace();  
+	        }  
+	}
+	
+	@SuppressWarnings("unchecked")
+	private ArrayList<Task> loadTasks(){
+	    ArrayList<Task> loadedTasks = new ArrayList<Task>();
+    	try {  
+        	FileInputStream fIn = context.openFileInput(FILE_NAME);
+            ObjectInputStream oIn = new ObjectInputStream(fIn);  
+            loadedTasks = (ArrayList<Task>) oIn.readObject();  
+            oIn.close(); 
+        } 
+         catch (IOException e) {  
+            e.printStackTrace();  
+        } catch (ClassNotFoundException e) {  
+            e.printStackTrace();  
+        }
+        return loadedTasks;
 	}
 }

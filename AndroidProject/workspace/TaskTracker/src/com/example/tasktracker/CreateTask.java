37130bf431365d19728f26d5c101c9390f332034
zturchan <zturchan@ualberta.ca>
2012-11-04 13:46:21 -0700

    Created listeners for createTask.  The save method needs to be implemented to interact with the appropriate managers.

--- AndroidProject/workspace/TaskTracker/src/com/example/tasktracker/CreateTask.java
+++ AndroidProject/workspace/TaskTracker/src/com/example/tasktracker/CreateTask.java
@@ -1,9 +1,10 @@
 package com.example.tasktracker;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 
 public class CreateTask extends Activity {
 
@@ -11,12 +12,34 @@ public class CreateTask extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_task);
+        
+        Button save = (Button) findViewById(R.id.button_create_save);
+        Button cancel = (Button) findViewById(R.id.button_create_cancel);
+        
+        save.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                setResult(RESULT_OK);
+                save(v);
+            }
+        });
+        cancel.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                setResult(RESULT_OK);
+                finish();
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_create_task, menu);
         return true;
+    }
+    
+    public void save(View v){
+        //Insert code to save the task locally.  Then if deemed public also save to service
+        
+        finish();
     }
     
     public void confirmTask(View view)

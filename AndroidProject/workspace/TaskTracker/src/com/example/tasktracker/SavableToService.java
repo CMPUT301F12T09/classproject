31892f8ed4daceecb1be20c734bb2f9c35b148ac
efauser <efauser@ualberta.ca>
2012-11-05 11:03:51 -0700

    Fixed crash when finished adding photos

--- AndroidProject/workspace/TaskTracker/src/com/example/tasktracker/SavableToService.java
+++ AndroidProject/workspace/TaskTracker/src/com/example/tasktracker/SavableToService.java
@@ -1,8 +1,12 @@
 package com.example.tasktracker;
 
+import java.io.Serializable;
 
-public class SavableToService
+
+public class SavableToService implements Serializable
 {
+	
+	private static final long serialVersionUID = 1L;
 	public SavableToService()
 	{
 	}

c904e41e9bab107ed9984fd701a4f95e15f8cd66
sgdavis <kingpreist@hotmail.com>
2012-12-02 19:03:17 -0700

    killing a debug line

--- AndroidProject/workspace/TaskTracker/src/controllers/ServiceManager.java
+++ AndroidProject/workspace/TaskTracker/src/controllers/ServiceManager.java
@@ -849,8 +849,6 @@ public class ServiceManager
 		    String delims = "[:,]+";
 		    String[] tokens = temp.split(delims);
 		    
-		    System.out.println("============================\n" + jsonStringVersion + "\n============================");
-		    
 		    for(int i = 0; i < tokens.length; i++)
 		    {
 		    	if(tokens[i].length() < 3)

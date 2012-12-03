8b50902c0047a1593b791c2dc34097583955f232
efauser <efauser@ualberta.ca>
2012-12-03 13:42:49 -0700

    Test Updates

--- AndroidProject/workspace/TaskTrackerTest/src/com/example/tasktracker/test/FulfillmentTest.java
+++ AndroidProject/workspace/TaskTrackerTest/src/com/example/tasktracker/test/FulfillmentTest.java
@@ -3,9 +3,7 @@ package com.example.tasktracker.test;
 import java.util.ArrayList;
 import java.util.Date;
 
-import com.example.tasktracker.AudioFile;
-import com.example.tasktracker.Fulfillment;
-import com.example.tasktracker.ImageFile;
+import model.*;
 
 
 import junit.framework.TestCase;
@@ -14,50 +12,38 @@ import junit.framework.TestCase;
 public class FulfillmentTest extends TestCase{
 	
 	public void testFulfillment(){
-		ArrayList<ImageFile> images = new ArrayList<ImageFile>();
-		ArrayList<AudioFile> audio = new ArrayList<AudioFile>();
-		Fulfillment f = new Fulfillment("12345678", "Some Text", images, audio);
+		Fulfillment f = new Fulfillment("12345678", "Some Text");
 		assertTrue(f.getUserDeviceId().equals("12345678"));
 		assertTrue(f.getTextInput().equals("Some Text"));
-		assertTrue(f.getImageFiles() == images);
-		assertTrue(f.getAudioFiles() == audio);
+		
 	}
 	
 	public void testGetUserDeviceId(){
-		ArrayList<ImageFile> images = new ArrayList<ImageFile>();
-		ArrayList<AudioFile> audio = new ArrayList<AudioFile>();
-		Fulfillment f = new Fulfillment("12345678", "Some Text", images, audio);
+		Fulfillment f = new Fulfillment("12345678", "Some Text");
 		assertTrue(f.getUserDeviceId().equals("12345678"));
 	}
 	
 	public void testSetUserDeviceId(){
-		ArrayList<ImageFile> images = new ArrayList<ImageFile>();
-		ArrayList<AudioFile> audio = new ArrayList<AudioFile>();
-		Fulfillment f = new Fulfillment("12345678", "Some Text", images, audio);
+
+		Fulfillment f = new Fulfillment("12345678", "Some Text");
 		f.setUserDeviceId("987654321");
 		assertTrue(f.getUserDeviceId().equals("987654321"));
 	}
 	
-	public void testGetTextInput(){
-		ArrayList<ImageFile> images = new ArrayList<ImageFile>();
-		ArrayList<AudioFile> audio = new ArrayList<AudioFile>();
-		Fulfillment f = new Fulfillment("12345678", "Some Text", images, audio);
+	public void testGetTextInput(){;
+		Fulfillment f = new Fulfillment("12345678", "Some Text");
 		assertTrue(f.getTextInput().equals("Some Text"));
 	}
 	
 	public void testSetTextInput(){
-		ArrayList<ImageFile> images = new ArrayList<ImageFile>();
-		ArrayList<AudioFile> audio = new ArrayList<AudioFile>();
-		Fulfillment f = new Fulfillment("12345678", "Some Text", images, audio);
+		Fulfillment f = new Fulfillment("12345678", "Some Text");
 		f.setTextInput("Different Text");
 		assertTrue(f.getTextInput().equals("Different Text"));
 	}
 	
 	@SuppressWarnings("deprecation")
 	public void testGetDateAdded(){
-		ArrayList<ImageFile> images = new ArrayList<ImageFile>();
-		ArrayList<AudioFile> audio = new ArrayList<AudioFile>();
-		Fulfillment f = new Fulfillment("12345678", "Some Text", images, audio);
+		Fulfillment f = new Fulfillment("12345678", "Some Text");
 		Date storedDate = f.getDateAdded();
 		Date today = new Date();
 		assertTrue(storedDate.getDate() == today.getDate());
@@ -66,9 +52,7 @@ public class FulfillmentTest extends TestCase{
 	}
 	
 	public void testSetDateAdded(){
-		ArrayList<ImageFile> images = new ArrayList<ImageFile>();
-		ArrayList<AudioFile> audio = new ArrayList<AudioFile>();
-		Fulfillment f = new Fulfillment("12345678", "Some Text", images, audio);
+		Fulfillment f = new Fulfillment("12345678", "Some Text");
 		Date newDate = new Date();
 		f.setDateAdded(newDate);
 		assertTrue(newDate == f.getDateAdded());
@@ -76,15 +60,16 @@ public class FulfillmentTest extends TestCase{
 	
 	public void testGetImageFiles(){
 		ArrayList<ImageFile> images = new ArrayList<ImageFile>();
-		ArrayList<AudioFile> audio = new ArrayList<AudioFile>();
-		Fulfillment f = new Fulfillment("12345678", "Some Text", images, audio);
-		assertTrue(f.getImageFiles() == images);
+		Fulfillment f = new Fulfillment("12345678", "Some Text");
+		ArrayList<ImageFile> newImages = new ArrayList<ImageFile>();
+		newImages.add(new ImageFile());
+		f.setImageFiles(newImages);
+		assertTrue(f.getImageFiles() == newImages);
+		
 	}
 	
 	public void testSetImageFiles(){
-		ArrayList<ImageFile> images = new ArrayList<ImageFile>();
-		ArrayList<AudioFile> audio = new ArrayList<AudioFile>();
-		Fulfillment f = new Fulfillment("12345678", "Some Text", images, audio);
+		Fulfillment f = new Fulfillment("12345678", "Some Text");
 		ImageFile i = new ImageFile();
 		ArrayList<ImageFile> newImages = new ArrayList<ImageFile>();
 		newImages.add(i);
@@ -93,16 +78,15 @@ public class FulfillmentTest extends TestCase{
 	}
 	
 	public void testGetAudioFiles(){
-		ArrayList<ImageFile> images = new ArrayList<ImageFile>();
-		ArrayList<AudioFile> audio = new ArrayList<AudioFile>();
-		Fulfillment f = new Fulfillment("12345678", "Some Text", images, audio);
-		assertTrue(f.getAudioFiles() == audio);
+		Fulfillment f = new Fulfillment("12345678", "Some Text");
+		ArrayList<AudioFile> newAudio = new ArrayList<AudioFile>();
+		newAudio.add(new AudioFile());
+		f.setAudioFiles(newAudio);
+		assertTrue(f.getAudioFiles() == newAudio);
 	}
 	
 	public void testSetAudioFiles(){
-		ArrayList<ImageFile> images = new ArrayList<ImageFile>();
-		ArrayList<AudioFile> audio = new ArrayList<AudioFile>();
-		Fulfillment f = new Fulfillment("12345678", "Some Text", images, audio);
+		Fulfillment f = new Fulfillment("12345678", "Some Text");
 		AudioFile a = new AudioFile();
 		ArrayList<AudioFile> newAudio = new ArrayList<AudioFile>();
 		newAudio.add(a);
@@ -111,21 +95,28 @@ public class FulfillmentTest extends TestCase{
 	}
 	
 	public void testAddImage(){
-		ArrayList<ImageFile> images = new ArrayList<ImageFile>();
-		ArrayList<AudioFile> audio = new ArrayList<AudioFile>();
-		Fulfillment f = new Fulfillment("12345678", "Some Text", images, audio);
+		Fulfillment f = new Fulfillment("12345678", "Some Text");
 		ImageFile i = new ImageFile();
 		f.addImage(i);
 		assertTrue(f.getImageFiles().contains(i));
 	}
 	
 	public void testAddAudio(){
-		ArrayList<ImageFile> images = new ArrayList<ImageFile>();
-		ArrayList<AudioFile> audio = new ArrayList<AudioFile>();
-		Fulfillment f = new Fulfillment("12345678", "Some Text", images, audio);
+		Fulfillment f = new Fulfillment("12345678", "Some Text");
 		AudioFile a = new AudioFile();
 		f.addAudio(a);
 		assertTrue(f.getAudioFiles().contains(a));
 	}
 	
+	public void testSetDbId(){
+		Fulfillment f = new Fulfillment("12345678", "Some Text");
+		f.setDbId(987654321);
+		assertTrue(f.getDbId() == 987654321);
+	}
+	
+	public void testGetDbId(){
+		Fulfillment f = new Fulfillment("12345678", "Some Text");
+		f.setDbId(987654321);
+		assertTrue(f.getDbId() == 987654321);
+	}
 }
--- AndroidProject/workspace/TaskTrackerTest/src/com/example/tasktracker/test/TaskTest.java
+++ AndroidProject/workspace/TaskTrackerTest/src/com/example/tasktracker/test/TaskTest.java
@@ -1,12 +1,7 @@
 package com.example.tasktracker.test;
 
 
-import java.util.ArrayList;
-
-import com.example.tasktracker.AudioFile;
-import com.example.tasktracker.Fulfillment;
-import com.example.tasktracker.ImageFile;
-import com.example.tasktracker.Task;
+import model.*;
 
 import junit.framework.TestCase;
 
@@ -15,7 +10,7 @@ public class TaskTest extends TestCase{
 
 	
 	public void testTaskStringStringStringBooleanBooleanBooleanBoolean(){
-		Task t =  new Task("12345678", "Test Name", "Test Description", true, true, true, true);
+		Task t =  new Task("12345678", "Test Name", "Test Description", true, true, true, true, "email@test.com");
 		assertTrue(t.getUserDeviceId().equals("12345678"));
 		assertTrue(t.getTaskName().equals("Test Name"));
 		assertTrue(t.getTaskDescription().equals("Test Description"));
@@ -27,122 +22,128 @@ public class TaskTest extends TestCase{
 	}
 	
 	public void testAddSubmission(){
-		Task task =  new Task("12345678", "Test Name", "Test Description", true, true, true, true);
-		ArrayList<ImageFile> images = new ArrayList<ImageFile>();
-		ArrayList<AudioFile> audio = new ArrayList<AudioFile>();
-		task.addSubmission("12345678", "Some Text", images, audio);
+		Task task =  new Task("12345678", "Test Name", "Test Description", true, true, true, true, "email@test.com");
+		Fulfillment f = new Fulfillment("12345678", "Some Text");
+		task.addSubmission(f);
 		assertTrue(task.getSubmissions().get(0).getUserDeviceId().equals("12345678"));
 	}
 	
 	public void testRemoveSubmission(){
 
-		Task task =  new Task("12345678", "Test Name", "Test Description", true, true, true, true);
-		ArrayList<ImageFile> images = new ArrayList<ImageFile>();
-		ArrayList<AudioFile> audio = new ArrayList<AudioFile>();
-		task.addSubmission("12345678", "Some Text", images, audio);
-		Fulfillment f = new Fulfillment("12345678", "Some Text", images, audio);
+		Task task =  new Task("12345678", "Test Name", "Test Description", true, true, true, true, "email@test.com");
+		Fulfillment f = new Fulfillment("12345678", "Some Text");
+		task.addSubmission(f);
 		task.removeSubmission(0);
 		assertFalse(task.getSubmissions().contains(f));
 	}
 
 	public void testGetUserDeviceId(){
-		Task t =  new Task("12345678", "Test Name", "Test Description", true, true, true, true);
+		Task t =  new Task("12345678", "Test Name", "Test Description", true, true, true, true, "email@test.com");
 		assertTrue(t.getUserDeviceId().equals("12345678"));
 	}
 	
 	public void testSetUserDeviceId(){
-		Task t =  new Task("12345678", "Test Name", "Test Description", true, true, true, true);
+		Task t =  new Task("12345678", "Test Name", "Test Description", true, true, true, true, "email@test.com");
 		t.setUserDeviceId("987654321");
 		assertTrue(t.getUserDeviceId().equals("987654321"));
 	}
 	
 	public void testGetTaskName(){
-		Task t =  new Task("12345678", "Test Name", "Test Description", true, true, true, true);
+		Task t =  new Task("12345678", "Test Name", "Test Description", true, true, true, true, "email@test.com");
 		assertTrue(t.getTaskName().equals("Test Name"));
 	}
 	
 	public void testSetTaskName(){
-		Task t =  new Task("12345678", "Test Name", "Test Description", true, true, true, true);
+		Task t =  new Task("12345678", "Test Name", "Test Description", true, true, true, true, "email@test.com");
 		t.setTaskName("New Name");
 		assertTrue(t.getTaskName().equals("New Name"));
 	}
 	
 	public void testGetTaskDescription(){
-		Task t =  new Task("12345678", "Test Name", "Test Description", true, true, true, true);
+		Task t =  new Task("12345678", "Test Name", "Test Description", true, true, true, true, "email@test.com");
 		assertTrue(t.getTaskDescription().equals("Test Description"));
 	}
 	
 	public void testSetTaskDescription(){
-		Task t =  new Task("12345678", "Test Name", "Test Description", true, true, true, true);
+		Task t =  new Task("12345678", "Test Name", "Test Description", true, true, true, true, "email@test.com");
 		t.setTaskDescription("New Description");
 		assertTrue(t.getTaskDescription().equals("New Description"));
 	}
 	
+	public void testGetEmail(){
+		Task t =  new Task("12345678", "Test Name", "Test Description", true, true, true, true, "email@test.com");
+		assertTrue(t.getEmail().equals("email@test.com"));
+	}
+	
+	public void testSetEmail(){
+		Task t =  new Task("12345678", "Test Name", "Test Description", true, true, true, true, "email@test.com");
+		t.setEmail("different@test.com");
+		assertTrue(t.getEmail().equals("different@test.com"));
+	}
+	
 	public void testGetWantPhoto(){
-		Task t =  new Task("12345678", "Test Name", "Test Description", false, true, false, false);
+		Task t =  new Task("12345678", "Test Name", "Test Description", false, true, false, false, "email@test.com");
 		assertTrue(t.getWantPhoto() == true);
 	}
 	
 	public void testSetWantPhoto(){
-		Task t =  new Task("12345678", "Test Name", "Test Description", false, false, false, false);
+		Task t =  new Task("12345678", "Test Name", "Test Description", false, false, false, false, "email@test.com");
 		t.setWantPhoto(true);
 		assertTrue(t.getWantPhoto() == true);
 	}
 	
 	public void testGetWantText(){
-		Task t =  new Task("12345678", "Test Name", "Test Description", true, false, false, false);
+		Task t =  new Task("12345678", "Test Name", "Test Description", true, false, false, false, "email@test.com");
 		assertTrue(t.getWantText() == true);
 	}
 	
 	public void testSetWantText(){
-		Task t =  new Task("12345678", "Test Name", "Test Description", false, false, false, false);
+		Task t =  new Task("12345678", "Test Name", "Test Description", false, false, false, false, "email@test.com");
 		t.setWantText(true);
 		assertTrue(t.getWantText() == true);
 	}
 	
 	public void testGetWantAudio(){
-		Task t =  new Task("12345678", "Test Name", "Test Description", false, false, true, false);
+		Task t =  new Task("12345678", "Test Name", "Test Description", false, false, true, false, "email@test.com");
 		assertTrue(t.getWantAudio() == true);
 	}
 	
 	public void testSetWantAudio(){
-		Task t =  new Task("12345678", "Test Name", "Test Description", false, false, false, false);
+		Task t =  new Task("12345678", "Test Name", "Test Description", false, false, false, false, "email@test.com");
 		t.setWantAudio(true);
 		assertTrue(t.getWantAudio() == true);
 	}
 	
 	public void testGetIsOpen(){
-		Task t =  new Task("12345678", "Test Name", "Test Description", false, false, false, false);
+		Task t =  new Task("12345678", "Test Name", "Test Description", false, false, false, false, "email@test.com");
 		assertTrue(t.getIsOpen() == true);
 	}
 	
 	public void testSetIsOpen(){
-		Task t =  new Task("12345678", "Test Name", "Test Description", false, false, false, false);
+		Task t =  new Task("12345678", "Test Name", "Test Description", false, false, false, false, "email@test.com");
 		t.setIsOpen(false);
 		assertTrue(t.getIsOpen() == false);
 	}
 	
 	public void testGetIsPublic(){
-		Task t =  new Task("12345678", "Test Name", "Test Description", false, false, false, true);
+		Task t =  new Task("12345678", "Test Name", "Test Description", false, false, false, true, "email@test.com");
 		assertTrue(t.getIsPublic() == true);
 	}
 	
 	public void testSetIsPublic(){
-		Task t =  new Task("12345678", "Test Name", "Test Description", false, false, false, false);
+		Task t =  new Task("12345678", "Test Name", "Test Description", false, false, false, false, "email@test.com");
 		t.setIsPublic(true);
 		assertTrue(t.getIsPublic() == true);
 	}
 	
 	public void testGetSubmissions(){
-		Task task =  new Task("12345678", "Test Name", "Test Description", true, true, true, true);
+		Task task =  new Task("12345678", "Test Name", "Test Description", true, true, true, true, "email@test.com");
 		assertTrue(task.getSubmissions().isEmpty());
 	}
 	
 	public void testAddFulfillment(){
-		Task task =  new Task("12345678", "Test Name", "Test Description", true, true, true, true);
-		ArrayList<ImageFile> images = new ArrayList<ImageFile>();
-		ArrayList<AudioFile> audio = new ArrayList<AudioFile>();
-		Fulfillment f = new Fulfillment("12345678", "Some Text", images, audio);
+		Task task =  new Task("12345678", "Test Name", "Test Description", true, true, true, true, "email@test.com");
+		Fulfillment f = new Fulfillment("12345678", "Some Text");
 		task.addFulfillment(f);
 		assertTrue(task.getSubmissions().contains(f));
 	}
